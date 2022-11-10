397f263ff14e92a5eaad0fd0759b8951dc71c12a
Jean Loberg Hansen <jeanloberg@gmail.com>
2022-11-10 19:50:11 +0100

    work on nested routes

--- pages/course.vue
+++ pages/course.vue
@@ -1,5 +1,21 @@
 <template>
-  <h1>My course</h1>
+  <main class="min-h-screen w-full bg-gray-100">
+    <div class="ml-auto mr-auto max-w-screen-lg">
+      <div class="pt-12 flex items-center flex-col w-full">
+        <div class="prose mb-12">
+          <h1>course:</h1>
+        </div>
+      </div>
+      <div class="flex flex-row space-x-8 flex-grow">
+        <aside class="bg-white w-3/12 p-5">
+          <p>Im a aside</p>
+        </aside>
+        <section class="bg-white w-full p-5">
+          <NuxtPage />
+        </section>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
---
+++ pages/course/lesson.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Im a lesson child</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
